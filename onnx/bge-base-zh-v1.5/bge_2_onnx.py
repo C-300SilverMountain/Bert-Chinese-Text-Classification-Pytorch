7f43c7033c81855b12299921808287168037debf
weiming.chen <weiming.chen@qizhidao.com>
2024-04-09 11:30:03 +0800

    feat: 代码结构优化

---
+++ onnx/bge-base-zh-v1.5/bge_2_onnx.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import onnx
+import onnxruntime as ort
+import torch
+from transformers import AutoModel
+from transformers import BertTokenizer, BertModel
+
+model_path = "model/BAAI/bge-base-zh-v1.5"
+MODEL_ONNX_PATH = "model/saved_dict/raw_bert_dynamic.onnx"
+OPERATOR_EXPORT_TYPE = torch._C._onnx.OperatorExportTypes.ONNX
+model = AutoModel.from_pretrained(model_path)
+model.eval()
+
+def build_args(seq_len):
+    org_input_ids = torch.LongTensor([[i for i in range(seq_len)]])
+    org_token_type_ids = torch.LongTensor([[1 for i in range(seq_len)]])
+    org_input_mask = torch.LongTensor(
+        [[0 for i in range(int(seq_len / 2))] + [1 for i in range(seq_len - int(seq_len / 2))]])
+    return org_input_ids, org_token_type_ids, org_input_mask
+
+def run_roberta_on_onnx(onnx_path):
+    tokenizer = BertTokenizer.from_pretrained("model/BAAI/bge-base-zh-v1.5")
+    sent = '样例数据-1'
+    tokenized_tokens = tokenizer(sent)
+    input_ids = np.array([tokenized_tokens['input_ids']], dtype=np.int64)
+    attention_mask = np.array([tokenized_tokens['attention_mask']], dtype=np.int64)
+    token_type_ids = np.array([tokenized_tokens['token_type_ids']], dtype=np.int64)
+
+    model = onnx.load(onnx_path)
+    sess = ort.InferenceSession(bytes(model.SerializeToString()))
+    result = sess.run(
+        output_names=None,
+        input_feed={"input_ids": input_ids,
+                    "attention_mask": attention_mask,
+                    "token_type_ids": token_type_ids}
+    )
+
+    last = result[0][:, 0]
+    sentence_embeddings = torch.nn.functional.normalize(torch.tensor(last), dim=-1)
+
+    print(sentence_embeddings)
+    # print(last)
+    # print(len(last[0]))
+
+def pytorch_roberta_2_onnx():
+    seq_len = 512
+    args = build_args(seq_len)
+    torch.onnx.export(model,
+                      args,
+                      MODEL_ONNX_PATH,
+                      verbose=True,
+                      operator_export_type=OPERATOR_EXPORT_TYPE,
+                      opset_version=12,
+                      input_names=['input_ids', 'attention_mask', 'token_type_ids'],  # 需要注意顺序！不可随意改变, 否则
+                      output_names=['last_hidden_state', 'pooler_output'],  # 需要注意顺序, 否则在推理阶段可能用错output
+                      do_constant_folding=True,
+                      dynamic_axes={"input_ids": {0: "batch_size", 1: "length"},
+                                    "token_type_ids": {0: "batch_size", 1: "length"},
+                                    "attention_mask": {0: "batch_size", 1: "length"},
+                                    "pooler_output": {0: "batch_size"},
+                                    "last_hidden_state": {0: "batch_size"}}
+                      )
+    print("Export of {} complete!".format(MODEL_ONNX_PATH))
+
+if __name__ == '__main__':
+    print('------')
+    # 将roberta转换成 onnx 格式
+    # pytorch_roberta_2_onnx()
+
+    # 在onnx环境下运行 roberta
+    run_roberta_on_onnx('model/saved_dict/raw_bert_dynamic.onnx')
---
+++ onnx/bge-base-zh-v1.5/run_by_onnx.py
@@ -0,0 +1,81 @@
+import os.path
+# from common import logger
+from transformers import AutoTokenizer
+import onnxruntime as ort
+from typing import List, Union, Any
+from tqdm import tqdm
+import torch
+import numpy as np
+
+
+class EmbModelEncoder:
+    def __init__(self, key: str = None, model_path: str = None, tag: str = None, b_normalize: bool = True):
+        self.key = key
+        self.model_path = model_path
+        self.tag = tag
+        # if len(model_path) == 0 or not os.path.exists(self.model_path):
+            # logger.error(f'模型文件不存在：${model_path}')
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.input_names = ['input_ids', 'attention_mask', 'token_type_ids']
+        self.ort_session = ort.InferenceSession(f'{model_path}/raw_bert_dynamic.onnx')
+        self.b_normalize = b_normalize
+
+    def to_numpy(self, tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    def encode(self, sentences: Union[str, List[str]], batch_size: int = 256, max_length: int = 512) \
+            -> Union[object, List[Any]]:
+        try:
+            r = self.encode_inner(sentences, batch_size, max_length).tolist()
+            # logger.info(f'编码长度为 {len(sentences)} : {len(r)}')
+            return r
+        except Exception as e:
+            # logger.error(f'模型编码错误: {sentences}', e)
+            return np.array([])
+
+    def encode_inner(self, sentences: Union[str, List[str]], batch_size: int = 256, max_length: int = 512) \
+            -> np.ndarray:
+        input_was_string = False
+        if isinstance(sentences, str):
+            sentences = [sentences]
+            input_was_string = True
+        all_embeddings = []
+        for start_index in tqdm(range(0, len(sentences), batch_size), desc="Embeddings", disable=len(sentences) < 256):
+            sentences_batch = sentences[start_index:start_index + batch_size]
+            inputs = self.tokenizer(
+                sentences_batch,
+                padding=True,
+                truncation=True,
+                return_tensors='pt',
+                max_length=max_length,
+            )
+            in_dict_numpy = {k: self.to_numpy(inputs[k]) for k in self.input_names}
+            ort_out_all = self.ort_session.run(
+                None,
+                in_dict_numpy,
+            )
+            ort_out_emb = ort_out_all[0][:, 0]
+            if self.b_normalize:
+                embeddings = torch.nn.functional.normalize(torch.tensor(ort_out_emb), dim=-1)
+            else:
+                embeddings = ort_out_emb
+            all_embeddings.append(embeddings.numpy())
+        all_embeddings = np.concatenate(all_embeddings, axis=0)
+        if input_was_string:
+            return all_embeddings[0]
+        return all_embeddings
+
+
+if __name__ == '__main__':
+    key = '111'
+    model_path = r'model/saved_dict'
+    tag = 'tag'
+    emb_encoder = EmbModelEncoder(key, model_path, tag)
+    one = emb_encoder.encode('你好，你叫什么名字')
+    print(one)
+    # text_list = []
+    # for one in range(1, 2):
+    #     text_list.append(f'语料附件独守空房睡觉了{one}')
+    # # text_list = ['福建省代理费', '放大了']
+    # ll = emb_encoder.encode(text_list)
+    # print(ll)
